178e88d80b82f465b3c4b3408b15abd55b2551f7
nyamothellonia <nyam.othellonia@gmail.com>
2018-10-30 18:46:33 +0900

    データ更新

---
+++ npm-scripts/crop-image.js
@@ -0,0 +1,30 @@
+const PNGCrop = require('png-crop');
+const fs = require('fs');
+
+const config = {width: 1125, height: 2202, top: 132, left: 0};
+const configIcon = {width: 288, height: 236, top: 1469, left: 8};
+
+fs.readdir('./tmp-image', function (err, files) {
+  if (err) throw err;
+  const fileList = [];
+  console.log(fileList);
+
+  files.forEach((itemPath) => {
+    const imgBuffer = fs.readFileSync('./tmp-image/' + itemPath);
+
+  if (!itemPath.includes('.png')) {
+    return;
+  }
+  console.log(itemPath);
+
+  PNGCrop.cropToStream(imgBuffer, config, function (err, outputStream) {
+    if (err) throw err;
+    outputStream.pipe(fs.createWriteStream(`tmp-image/dist/chara-image-list/${itemPath}`));
+  });
+
+  PNGCrop.cropToStream(imgBuffer, configIcon, function (err, outputStream) {
+    if (err) throw err;
+    outputStream.pipe(fs.createWriteStream(`tmp-image/dist/chara-image-icon/${itemPath}`));
+  });
+});
+});
